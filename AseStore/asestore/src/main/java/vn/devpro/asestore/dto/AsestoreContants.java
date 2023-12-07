52831a966146e96315c16f5f8b6032e237738987
LOGAN-Duc <110994669+LOGAN-Duc@users.noreply.github.com>
2023-12-07 14:43:48 +0700

    first commit

--- AseStore/asestore/src/main/java/vn/devpro/asestore/dto/AsestoreContants.java
+++
@@ -1,7 +0,0 @@
-package vn.devpro.asestore.dto;
-
-public interface AsestoreContants {
-	public final String FOLER_UPLOAD = "F:/LearnJavaWeb/Devpro/JavaSpringBoots/homework/AseStore/asestore/FileUploads/";
-	public final int SIZE_OF_PAGE = 3;
-	public final int SIZE_OF_PAGE1 = 15;
-}
--- AseStore/asestore/src/main/webapp/WEB-INF/views/common/variables.jsp
+++
@@ -1,4 +0,0 @@
-<!-- JSTL directive -->
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-
-<c:set var="classpath" value="${pageContext.servletContext.contextPath}"></c:set>
